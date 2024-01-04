164c66ca8c92ddd54d7adc31229e60d6e7782b38
Glitch (glitch-hello-node) <none>
2024-01-04 06:06:39 +0000

    🍩🛬 Checkpoint ./index.js:62000883/2789 ./commands/test.ts:62000883/712

---
+++ commands/test.js
@@ -0,0 +1,25 @@
+const discord = require('discord.js');
+
+module.exports = {
+  data: new discord.SlashCommandBuilder()
+    .setName('verify')
+    .setDescription('認証パネルを配置')
+    .setDefaultMemberPermissions(discord.PermissionFlagsBits.Administrator)
+  ,
+  async execute(interaction) {
+
+    const embed = new discord.EmbedBuilder()
+      .setColor(process.env.COLOR)
+      .setTitle('Verify')
+      .setDescription('下記ボタンから認証してください')
+
+    const button = new discord.ButtonBuilder()
+      .setLabel('✅認証')
+      .setURL('https://example.com')
+      .setStyle(discord.ButtonStyle.Link);
+
+    const row = new discord.ActionRowBuilder().addComponents(button);
+
+    await interaction.reply({ embeds: [embed], components: [row] });
+  }
+}
--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 const discord = require('discord.js');
+const fs = require('fs');
+
+const client = new discord.Client({
+  intents: [
+    discord.GatewayIntentBits.Guilds,
+    discord.GatewayIntentBits.GuildMessages,
+    // 他の必要なインテントを追加
+  ],
+});
+
+client.on('ready', () => {
+  console.log(`ok`);
+});
+
+// 以下はイベントの例です
+client.on('messageCreate', (message) => {
+  console.log(`Received message: ${message}`);
+});
+
+// コマンドデータの取得
+const commands = new discord.Collection();
 
 
-const express = require('express');
-const app = express();
+  fs.readdirSync('commands')
+    .filter(file => file.endsWith('.js'))
+    .forEach(file => {
+      const command = require(`./commands/${file}`);
+      commands[command.data.name] = command;
+    });
 
 
-app.get('/', (req, res) => {
-  res.json('Not found');
+//コマンドを登録
+client.once(discord.Events.ClientReady, async() => {
+  const data = [];
+  for (const commandName in commands) {
+    data.push(commands[commandName].data)
+  }
+  await client.application.commands.set(data);
+  client.guilds.cache.forEach(async (guild) => {
+    console.log(`NAME: ${guild.name}, ID: ${guild.id}`);
+  });
 });
 
 
-app.listen(3000);
+// コマンドを実行
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  try {
+    await commands[interaction.commandName].execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: `CommandExcusionError\`\`\`${error}\`\`\``,
+      ephemeral: true,
+    })
+  }
+});
+
+
+// イベントを登録
+const events = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
+
+for (const event of events) {
+  const data = require(`./events/${event}`);
+  if (data.once) {
+    client.once(data.name, (...args) => data.execute(...args));
+  } else {
+    client.on(data.name, (...args) => data.execute(...args));
+  }
+}
+
+
+client.login('MTE5MjMwMDA0MDE2MTYwNzc0MQ.GvJiyr.PLr00HnnBx0ikj7ZxC51ZoCC-H0pBJjxjWH_eQ');
