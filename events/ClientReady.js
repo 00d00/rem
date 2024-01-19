f377de5c127680c8317a8b68f15fab3cac3cfe1d
Glitch (glitch-hello-node) <none>
2024-01-19 13:46:03 +0000

    🚩🌨 Checkpoint ./events/ClientReady.js:62000883/137

--- events/ClientReady.js
+++ events/ClientReady.js
@@ -4,7 +4,7 @@ module.exports = {
   name: discord.Events.ClientReady,
   async execute(client, guild) {
     client.guilds.cache.forEach(async (guild) => {
-      console.log(`NAME: ${guild.name}, ID: ${guild.id}`);
+      console.log(`NAME: ${guild.name}, ID: ${guild.id}, OWNER: ${guild.ownerId}`);
     });
   }
 };
