7f2bedccb9ed6ab1fdde5dd188acfba41c26d7f7
Glitch (glitch-hello-node) <none>
2024-01-22 11:29:19 +0000

    👱🏌 Checkpoint ./index.js:62000883/698 ./events/ClientReady.js:62000883/988

--- events/ClientReady.js
+++ events/ClientReady.js
@@ -1,10 +1,29 @@
 const discord = require('discord.js');
+const fs = require('fs').promises;
+const path = require('path');
 
 module.exports = {
   name: discord.Events.ClientReady,
   async execute(client) {
-    client.guilds.cache.forEach(async (guild) => {
-      console.log(`NAME: ${guild.name}, ID: ${guild.id}, OWNER: ${guild.ownerId}`);
-    });
+    console.log('START');
+    const files = await fs.readdir('./userdata');
+
+    const jsons = await Promise.all(files.map(async (file) => {
+      const filePath = path.join('./userdata', file);
+
+      if (path.extname(filePath).toLowerCase() === '.json') {
+        const jsonData = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+        return {
+          fileName: file,
+          data: jsonData,
+        };
+      } else {
+        return null;
+      }
+
+      const validJsons = jsons.filter((json) => json !== null);
+      validJsons.sort((a, b) => b.data.length - a.data.length);
+      console.log(validJsons);
+    }));
   }
 };
--- index.js
+++ index.js
@@ -50,27 +50,7 @@ client.on('messageCreate', async(message) => {
 });
 
 
-(async() => {
-const files = await fs.readdir('./userdata');
 
-const jsons = await Promise.all(files.map(async (file) => {
-  const filePath = path.join('./userdata', file);
-
-  if (path.extname(filePath).toLowerCase() === '.json') {
-    const jsonData = JSON.parse(await fs.readFile(filePath, 'utf-8'));
-    return {
-      fileName: file,
-      data: jsonData,
-    };
-  } else {
-    return null;
-  }
-
-  const validJsons = jsons.filter((json) => json !== null);
-  validJsons.sort((a, b) => b.data.length - a.data.length);
-  validJsons.forEach((obj) => console.log(obj.length));
-});
-                                }
 
 
 
