2658b2d9f57fa27404a62a659d8837e8812785df
Glitch (discord-auth-system) <none>
2024-02-12 04:24:53 +0000

    🌾🏘 Checkpoint ./events/role.js:62000883/758

--- events/role.js
+++ events/role.js
@@ -3,7 +3,12 @@ import discord from 'discord.js';
 export default {
   name: discord.Events.MessageReactionAdd,
   async execute(reaction, user) {
-    if (user.id === process.env.CLIENT_ID) return;
+    if (user.bot) return;
+    if (reaction.message.author.id !== process.env.CLIENT_ID) return;
+
+    const match = reaction.message.content.match(new RegExp(`${reaction.emoji.name}\\s:\\s<@&\\d+>`));
+
+    if (!match) return;
 
     if (reaction.emoji.name === '1⃣') {
       reaction.message.channel.send(`${user.tag} が1⃣にリアクションしました！`);
