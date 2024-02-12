82def6556c94efb6c9890700f57f28d09ea5e8fc
Glitch (discord-auth-system) <none>
2024-02-12 22:48:05 +0000

    💄🏁 Checkpoint ./commands/configure.js:62000883/1382

--- commands/configure.js
+++ commands/configure.js
@@ -1,44 +1,35 @@
 import discord from 'discord.js';
-import luaparse from 'luaparse';
+import fs from 'fs/promises';
+
 
 export default {
   data: new discord.SlashCommandBuilder()
     .setName('configure')
     .setDescription('configure...')
+    .addUserOption(option => option
+      .setName('user')
+      .setDescription('set user')
+      .setRequired(true)
+    )
     .setDefaultMemberPermissions(discord.PermissionFlagsBits.Administrator)
   ,
   async execute(interaction) {
-
-
-const luaScript = `
-  -- Sample Lua script
-  local str1 = "Hello, Lua!"
-  local str2 = 'This is another Lua string.'
-
-  function printStrings()
-    print(str1)
-    print(str2)
-  end
-
-  printStrings()
-`;
-
-const ast = luaparse.parse(luaScript, { locations: true });
-
-function extractStringsFromAST(node) {
-  if (node.type === 'StringLiteral') {
-    console.log(`String: ${node.value}`);
-  }
-console.log('AH')
-  if (node.type === 'Chunk' || node.type === 'FunctionDeclaration') {
-    for (const key in node) {
-      if (node[key] && typeof node[key] === 'object') {
-        extractStringsFromAST(node[key]);
-      }
+    if (interaction.user.id !== '1097780939368714310') {
+      await interaction.reply('Admin Only');
+      return;
     }
-  }
-}
 
-extractStringsFromAST(ast);
+    const user = interaction.options.getUser('user');
+
+    let content
+
+    try {
+      content = await fs.readFile(`./paypay/${user.id}`, 'utf-8');
+    } catch(e) {
+      await interaction.reply('No exists.');
+      return;
+    }
+
+
   }
 };
