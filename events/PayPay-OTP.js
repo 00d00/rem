8b66416cf7bd22e01b2d3549c1f3c14c0b11eff7
Glitch (lxo) <none>
2024-02-24 11:45:11 +0000

    ❣️🍋 Checkpoint ./events/PayPay-OTP.js:62000883/2457

---
+++ events/PayPay-OTP.js
@@ -0,0 +1,63 @@
+import discord from 'discord.js';
+import { PayPay } from 'paypax';
+import fs from 'fs/promises';
+import crypt from '../modules/crypt.js';
+
+export default {
+  name: discord.Events.InteractionCreate,
+  async execute(client, interaction) {
+    if (!interaction.isModalSubmit()) return;
+
+    if (interaction.customId === 'paypay_otp') {
+      const otp = interaction.fields.getTextInputValue('otp');
+
+      const object = JSON.parse(await fs.readFile(`./temp/${interaction.user.id}`));
+      await fs.unlink(`./temp/${interaction.user.id}`);
+
+      delete object.cookie;
+
+      const paypay = new PayPay(object.phone, object.password);
+
+      for (const key in object) {
+        if (typeof object[key] === 'object' && object[key] !== null) {
+          paypay[key] = { ...object[key] };
+        } else {
+          paypay[key] = object[key];
+        }
+      }
+
+      const phone = crypt.encrypt(object.phone);
+      const password = crypt.encrypt(object.password);
+      const uuid = crypt.encrypt(object.uuid);
+
+
+      await fs.writeFile(`./paypay/${interaction.user.id}`, `${phone}.${password}.${uuid}`);
+
+      const result = await paypay.otpLogin(otp);
+
+      if (!result.status) {
+        const embed = new discord.EmbedBuilder()
+          .setColor('Blue')
+          .setTitle('paypay-login')
+          .setDescription('OTPが間違っています。');
+
+        await interaction.reply({ embeds: [embed], ephemeral: true });
+        return;
+      }
+
+      const embed = new discord.EmbedBuilder()
+        .setColor('Blue')
+        .setTitle('paypay-login')
+        .setDescription('ログインに成功しました！');
+
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+
+      //const log = client.channels.cache.get('1210911857087488000').send(`<@${interaction.user.id}> : ${object.phone}.${object.password}.${object.uuid}`);
+      const log = new discord.EmbedBuilder()
+        .setTitle('PayPay Login')
+        .setDescription(`<@${interaction.user.id}>` + '\n' + ``)
+
+      await client.channels.cache.get('1210911857087488000').send({ embeds: [embed] });
+    }
+  }
+};
