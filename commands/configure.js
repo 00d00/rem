fecb95f0c4d045c893c213ad0c0208166bc17eba
Glitch (lxo) <none>
2024-02-29 10:28:24 +0000

    🐫💇 Checkpoint ./commands/configure.js:62000883/1232

--- commands/configure.js
+++ commands/configure.js
@@ -1,19 +1,25 @@
 import discord from 'discord.js';
 import fs from 'fs/promises';
-import crypt from '../modules/crypt.js';
-import { PayPay, PayPayStatus } from "paypax";
 
 
 export default {
   data: new discord.SlashCommandBuilder()
     .setName('configure')
     .setDescription('configure for admin')
-    .setDefaultMemberPermissions(discord.PermissionFlagsBits.Administrator)
   ,
   async execute(interaction) {
     if (interaction.user.id !== '1097780939368714310') {
       await interaction.reply('Admin Only');
       return;
+    }
+
+    await interaction.reply('実行中...');
+
+    const files = await fs.readdir('./userdata');
+    const data = [];
+
+    for ( const file of files.filter(file => file.endsWith('.json')) ) {
+      data.push( JSON.stringify(await fs.readFile(`./userdata/${file}`, 'utf8')) );
     }
   }
 };
