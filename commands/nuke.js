38ee208af29793784cae1e04d8c5428fdbabf720
Glitch (lxo) <none>
2024-03-01 02:22:43 +0000

    👑👘 Checkpoint ./commands/nuke.js:62000883/6455

--- commands/nuke.js
+++ commands/nuke.js
@@ -1,27 +1,27 @@
-import discord from "discord.js";
+import discord from 'discord.js';
 
 export default {
   data: new discord.SlashCommandBuilder()
-    .setName("nuke")
-    .setDescription("チャンネル履歴を削除")
+    .setName('nuke')
+    .setDescription('チャンネル履歴を削除')
     .setDefaultMemberPermissions(discord.PermissionFlagsBits.Administrator)
   ,
   async execute(interaction) {
     const confirm = new discord.ButtonBuilder()
-      .setCustomId("nuke_confirm")
-      .setLabel("削除する")
+      .setCustomId('nuke_confirm')
+      .setLabel('削除する')
       .setStyle(discord.ButtonStyle.Danger);
 
     const cancel = new discord.ButtonBuilder()
-      .setCustomId("nuke_cancel")
-      .setLabel("キャンセル")
+      .setCustomId('nuke_cancel')
+      .setLabel('キャンセル')
       .setStyle(discord.ButtonStyle.Secondary);
 
     const row = new discord.ActionRowBuilder().addComponents(confirm, cancel);
 
     const embed = new discord.EmbedBuilder()
-      .setTitle("nuke")
-      .setDescription("本当にチャンネル履歴を削除しますか？");
+      .setTitle('nuke')
+      .setDescription('本当にチャンネル履歴を削除しますか？');
 
     const res = await interaction.reply({ embeds: [embed], components: [row] });
 
@@ -31,67 +31,67 @@ export default {
         time: 60000,
       });
 
-      if (confirmation.customId === "nuke_confirm") {
+      if (confirmation.customId === 'nuke_confirm') {
         const channel = await interaction.channel.clone();
         await channel.setPosition(interaction.channel.position);
         await interaction.channel.delete();
 
         const ad = new discord.ButtonBuilder()
-          .setURL("https://discord.com/api/oauth2/authorize?client_id=1191234193099849838&permissions=8&scope=bot")
-          .setLabel("Acesを招待")
+          .setURL('https://discord.com/api/oauth2/authorize?client_id=1191234193099849838&permissions=8&scope=bot')
+          .setLabel('Acesを招待')
           .setStyle(discord.ButtonStyle.Link);
 
         const row = new discord.ActionRowBuilder().addComponents(ad);
 
         const embed = new discord.EmbedBuilder()
-          .setTitle("nuke")
+          .setTitle('nuke')
           .setDescription(`<@${interaction.user.id}> が/nukeを実行しました。`);
 
         await channel.send({ embeds: [embed], components: [row] });
 
-      } else if (confirmation.customId === "nuke_cancel") {
+      } else if (confirmation.customId === 'nuke_cancel') {
         const confirm = new discord.ButtonBuilder()
-          .setCustomId("nuke_confirm")
-          .setLabel("削除する")
+          .setCustomId('nuke_confirm')
+          .setLabel('削除する')
           .setStyle(discord.ButtonStyle.Danger)
           .setDisabled(true);
 
         const cancel = new discord.ButtonBuilder()
-          .setCustomId("nuke_cancel")
-          .setLabel("キャンセル")
+          .setCustomId('nuke_cancel')
+          .setLabel('キャンセル')
           .setStyle(discord.ButtonStyle.Secondary)
           .setDisabled(true);
 
         const row = new discord.ActionRowBuilder().addComponents(confirm, cancel);
 
         const embed = new discord.EmbedBuilder()
-          .setTitle("nuke")
-          .setDescription("キャンセルしました。");
+          .setTitle('nuke')
+          .setDescription('キャンセルしました。');
 
         confirmation.reply({ embeds: [embed], components: [row] });
         await res.delete();
       }
     } catch (error) {
       const confirm = new discord.ButtonBuilder()
-        .setCustomId("nuke_confirm")
-        .setLabel("削除する")
+        .setCustomId('nuke_confirm')
+        .setLabel('削除する')
         .setStyle(discord.ButtonStyle.Danger)
         .setDisabled(true);
 
       const cancel = new discord.ButtonBuilder()
-        .setCustomId("nuke_cancel")
-        .setLabel("キャンセル")
+        .setCustomId('nuke_cancel')
+        .setLabel('キャンセル')
         .setStyle(discord.ButtonStyle.Secondary)
         .setDisabled(true);
 
       const row = new discord.ActionRowBuilder().addComponents(confirm, cancel);
 
       const embed = new discord.EmbedBuilder()
-        .setTitle("nuke")
-        .setDescription("タイムアウトしました。");
+        .setTitle('nuke')
+        .setDescription('タイムアウトしました。');
 
       interaction.followUp({ embeds: [embed], components: [row]});
       await res.delete();
     }
-  },
+  }
 };
