e7bea4d378de65dac02b3023afb041a388ca7b6e
Glitch (x0p) <none>
2024-03-09 23:41:33 +0000

    🚋🗾 Checkpoint ./commands/restore.js:62000883/545

--- commands/restore.js
+++ commands/restore.js
@@ -52,6 +52,22 @@ export default {
         code429: 0, // Too Many Request
       };
 
+      const postData = {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: jsonData[key].refreshToken,
+        redirect_uri: 'https://dis-auth.glitch.me/oauth',
+      };
+
+const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+};
+
+// データを直接渡す
+const response = await axios.post('https://discord.com/api/v10/oauth2/token', new URLSearchParams(postData), {
+  headers: headers,
+});
 
     }
   }
