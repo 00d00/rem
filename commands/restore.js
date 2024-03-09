ef99f8bdd4d2d84a8decaf0c5ae32323f4bc7c01
Glitch (x0p) <none>
2024-03-09 23:18:31 +0000

    🔨😻 Checkpoint ./commands/restore.js:62000883/2967

--- commands/restore.js
+++ commands/restore.js
@@ -6,25 +6,24 @@ import crypt from '../modules/crypt.js';
 
 export default {
   data: new discord.SlashCommandBuilder()
-    .setName('refresh')
-    .setDescription('認証者を再取得')
+    .setName('restore')
+    .setDescription('認証者を復元')
     .addStringOption((option) => option
-      .setName("パスワード")
+      .setName('パスワード')
       .setDescription('パスワードを入力')
       .setRequired(true)
     )
     .addIntegerOption((option) => option
-      .setName("登録id")
+      .setName('登録id')
       .setDescription('IDを指定')
       .setRequired(true)
     )
     .setDefaultMemberPermissions(discord.PermissionFlagsBits.Administrator)
   ,
   async execute(interaction) {
-    let saveId = interaction.options.getInteger('登録id');
+    const saveId = interaction.options.getInteger('登録id').toString();
     const password = interaction.options.getString('パスワード');
 
-    saveId = saveId.toString();
 
     let jsonData
 
@@ -35,7 +34,7 @@ export default {
       return;
     }
 
-    await interaction.reply({ content: '再取得中...', ephemeral: true });
+    await interaction.reply({ content: '認証者を復元します。', ephemeral: true });
 
     for (const key in jsonData) {
       const token = jsonData[key];
