09d5fb9344658e98fff69a8fb4e90dfb13867d6c
Glitch (x0p) <none>
2024-03-17 12:01:07 +0000

    😬🏈 Checkpoint ./commands/poll.js:62000883/2844

---
+++ commands/poll.js
@@ -0,0 +1,60 @@
+import discord from 'discord.js';
+
+
+export default {
+  data: new discord.SlashCommandBuilder()
+    .setName('poll')
+    .setDescription('アンケートパネルを設置')
+    .addStringOption(option => option
+      .setName('title')
+      .setDescription('タイトル')
+      .setRequired(true)
+    )
+    .addStringOption(option => option
+      .setName('choice_a')
+      .setDescription('選択肢A')
+      .setRequired(false)
+    )
+    .addStringOption(option => option
+      .setName('choice_b')
+      .setDescription('選択肢B')
+      .setRequired(false)
+    )
+    .addStringOption(option => option
+      .setName('choice_c')
+      .setDescription('選択肢C')
+      .setRequired(false)
+    )
+    .addStringOption(option => option
+      .setName('choice_d')
+      .setDescription('選択肢D')
+      .setRequired(false)
+    )
+    .addStringOption(option => option
+      .setName('choice_e')
+      .setDescription('選択肢E')
+      .setRequired(false)
+    )
+  ,
+  async execute(interaction) {
+    const title = interaction.options.getString('title');
+
+    const choice = [
+      interaction.options.getString('choice_a'),
+      interaction.options.getString('choice_b'),
+      interaction.options.getString('choice_c'),
+      interaction.options.getString('choice_d'),
+      interaction.options.getString('choice_e'),
+    ];
+
+  
+    const embed = new discord.EmbedBuilder()
+      .setColor('Blue')
+      .setTitle()
+      .setDescription()
+    interaction.reply({
+      content: `:bar_chart: ${title}`,
+      embeds: [embed]
+    });
+  }
+};
