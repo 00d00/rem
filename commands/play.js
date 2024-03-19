e01a0968dcd7b16c00008ed7fd9b5c2268912d0e
Glitch (x0p) <none>
2024-03-19 01:07:25 +0000

    🏭🍞 Checkpoint ./commands/play.js:62000883/4208

--- commands/play.js
+++ commands/play.js
@@ -5,57 +5,57 @@ import ytdl from 'ytdl-core';
 import ytsr from 'ytsr';
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName('play')
-        .setDescription('音楽を再生します')
-        .addStringOption(option => option
-          .setName('keyword')
-          .setDescription('キーワードまたはurl')
-          .setRequired(true)
-        )
-    ,
-    async execute(interaction) {
-        const keyword = interaction.options.getString('keyword');
+  data: new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('音楽を再生します')
+    .addStringOption(option => option
+      .setName('keyword')
+      .setDescription('キーワードまたはurl')
+      .setRequired(true)
+    )
+  ,
+  async execute(interaction) {
+    const keyword = interaction.options.getString('keyword');
 
-        const filters = await ytsr.getFilters(keyword);
-        const filter = filters.get('Type').get('Video');
-        const searchResults = await ytsr(filter.url, { limit: 5 });
+    const filters = await ytsr.getFilters(keyword);
+    const filter = filters.get('Type').get('Video');
+    const searchResults = await ytsr(filter.url, { limit: 5 });
 
-        const url = searchResults.items[0].url;
-        if (!ytdl.validateURL(url)) return interaction.reply(`${url}は処理できません。`);
+    const url = searchResults.items[0].url;
+    if (!ytdl.validateURL(url)) return interaction.reply(`${url}は処理できません。`);
 
-        const member = interaction.member;
-        if (!member || !member.voice.channel) {
-            return await interaction.reply({
-                content: 'ボイスチャンネルに参加してください',
-                ephemeral: true,
-            });
-        }
-
-        const connection = joinVoiceChannel({
-            adapterCreator: member.voice.channel.guild.voiceAdapterCreator,
-            channelId: member.voice.channel.id,
-            guildId: member.voice.channel.guild.id,
-            selfDeaf: true,
-            selfMute: false
-        });
-
-        await interaction.reply(url+"\n上記のURLを再生します");
-        const player = createAudioPlayer();
-        connection.subscribe(player);
-
-        const stream = ytdl(ytdl.getURLVideoID(url), {
-            filter: format => format.audioCodec === 'opus' && format.container === 'webm',
-            quality: 'highest',
-            highWaterMark: 32 * 1024 * 1024,
-        });
-        const resource = createAudioResource(stream, {
-            inputType: StreamType.WebmOpus
-        });
-
-        player.play(resource);
-        await entersState(player,AudioPlayerStatus.Playing, 10 * 1000);
-        await entersState(player,AudioPlayerStatus.Idle, 24 * 60 * 60 * 1000);
-        connection.destroy();
+    const member = interaction.member;
+    if (!member || !member.voice.channel) {
+      return await interaction.reply({
+        content: 'ボイスチャンネルに参加してください',
+        ephemeral: true,
+      });
     }
+
+    const connection = joinVoiceChannel({
+      adapterCreator: member.voice.channel.guild.voiceAdapterCreator,
+      channelId: member.voice.channel.id,
+      guildId: member.voice.channel.guild.id,
+      selfDeaf: true,
+      selfMute: false
+    });
+
+    await interaction.reply(url+"\n上記のURLを再生します");
+    const player = createAudioPlayer();
+    connection.subscribe(player);
+
+    const stream = ytdl(ytdl.getURLVideoID(url), {
+      filter: format => format.audioCodec === 'opus' && format.container === 'webm',
+      quality: 'highest',
+      highWaterMark: 32 * 1024 * 1024,
+    });
+    const resource = createAudioResource(stream, {
+      inputType: StreamType.WebmOpus
+    });
+
+    player.play(resource);
+    await entersState(player,AudioPlayerStatus.Playing, 10 * 1000);
+    await entersState(player,AudioPlayerStatus.Idle, 24 * 60 * 60 * 1000);
+    connection.destroy();
+  }
 }
