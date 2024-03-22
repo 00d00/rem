ce5055077a122532ee3323397e7b39ca3fbd49df
Glitch (x0p) <none>
2024-03-22 00:19:53 +0000

    🐝👛 Checkpoint ./commands/shop.js:62000883/809

---
+++ commands/shop.js
@@ -0,0 +1,29 @@
+import discord from 'discord.js';
+
+
+export default {
+  data: new discord.SlashCommandBuilder()
+    .setName('shop')
+    .setDescription('shopパネルを作成します')
+  ,
+  async execute(interaction) {
+    const embed = new discord.EmbedBuilder()
+      .setColor('Blue')
+      .setTitle('Backup Help')
+      .setDescription(
+        '**/verify [password] [id]**\n' +
+        '認証パネルを設置します。idはある場合指定することができます。\n' +
+        '生成されたidとパスワードは忘れないようにしてください。\n' +
+        '\n' +
+        '**/restore [password] [id]**\n' +
+        'メンバーを復元します。\n' +
+        '\n' +
+        '**/count [passord] [id]**\n' +
+        '認証者数を表示します。\n' +
+        '\n' +
+        '**/ranking**\n' +
+        '認証者数ランキングを見れます。'
+      )
+    interaction.reply({ embeds: [embed] });
+  }
+};
