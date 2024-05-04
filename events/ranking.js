a3b2e7b2ca5052ce563aa165ca8e1d9a861c35a7
Glitch (x0p) <none>
2024-05-04 03:48:03 +0000

    🍘🐾 Checkpoint ./events/verify.js:62000883/779 ./events/ranking.js:62000883/18

--- events/ranking.js
+++ events/ranking.js
@@ -2,7 +2,7 @@ import discord from 'discord.js';
 
 export default {
   name: discord.Events.InteractionCreate,
-  async execute(interaction) {
+  async execute(client, interaction) {
     if (interaction.customId === 'ranking-back') {
       
     } else if (interaction.customId === 'ranking-next') {
