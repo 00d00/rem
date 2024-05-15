47c0f17df1deac015c36aae44cd5c480011d8b8b
Glitch (x0p) <none>
2024-05-15 01:24:26 +0000

    👜🔖 Checkpoint ./events/shop-events/edit_item.js:62000883/538

--- events/shop-events/edit_item.js
+++ events/shop-events/edit_item.js
@@ -5,38 +5,35 @@ import newItemSelect from './newItemSelect.js';
 
 export async function edit_item(interaction, shop) {
   const row = newItemSelect(interaction, shop);
-  const message = await interaction.reply({ content: 'Components', components: [row], ephemeral: true });
-
-const callback = async (i) => {
-  if (i.user.id !== interaction.user.id) return;
-  if (i.customId !== 'item_select') return;
-
-  const itemName = i.values[0];
-
   const modal = newModal({
     id: 'modal',
     title: '商品編集',
     input: [
       {
-        label: '商品名',
+        label: '変更する商品名',
         id: 'name',
         style: discord.TextInputStyle.Short
       },
       {
-        label: '値段',
-        id: 'price',
+        label: '新しい商品名',
+        id: 'new_name',
+        style: discord.TextInputStyle.Short
+      },
+      {
+        label: '新しい値段',
+        id: 'new_price',
         style: discord.TextInputStyle.Short
       }
     ]
   });
 
-  await i.showModal(modal);
+  await interaction.showModal(modal);
   let response;
 
   try {
-    response = await i.awaitModalSubmit({ time: 180000 });
+    response = await interaction.awaitModalSubmit({ time: 180000 });
   } catch (error) {
-    await i.followUp({ content: 'タイムアウトしました。', ephemeral: true });
+    await interaction.followUp({ content: 'タイムアウトしました。', ephemeral: true });
     return;
   }
 
@@ -66,7 +63,6 @@ const callback = async (i) => {
     .setDescription(`商品名:${inputName}\n値段:${inputPrice}`);
 
   await response.reply({ embeds: [embed], ephemeral: true });
-};
 
   interaction.client.on(discord.Events.InteractionCreate, callback);
 
