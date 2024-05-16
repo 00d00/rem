db61775501ae7c6e94cb53341feebf2855d73888
Glitch (x0p) <none>
2024-05-16 01:28:25 +0000

    💾🎽 Checkpoint ./events/GuildMemberAdd.js:62000883/265

--- events/GuildMemberAdd.js
+++ events/GuildMemberAdd.js
@@ -1,16 +1,16 @@
 import discord from 'discord.js';
 
 export default {
-  name: discord.Events.InteractionCreate,
+  name: discord.Events.GuildMemberAdd,
   async execute(client, member) {
-    const channel = member.guild.systemChannel; // 離脱通知用のチャネルを指定します
+    const channel = member.guild.xx;
     if (!channel) return;
 
-    const embed = new disocrd.EmbedBuilder()
+    const embed = new discord.EmbedBuilder()
       .setColor(0xFF0000)
-      .setTitle('LEAVE - 退出')
-      .setDescription(`<@${member.id}> がサーバーから脱退しました。\n現在のメンバー数: ${member.guild.memberCount} 人`)
-      .setFooter({ text: 'MADE BY TETTU0530' });
+      .setTitle('LEAVE LOG')
+      .setDescription(`<@${member.id}> が退出しました。`)
+      .setFooter({ text: `現在${member.guild.memberCount}人` });
 
     await channel.send({ embeds: [embed] });
   }
