d02c07a73c7a6a629a197aa386067d1c49ae5b66
Glitch (x0p) <none>
2024-05-18 07:08:50 +0000

    🍡🕍 Checkpoint ./modules/stake.js:62000883/222

--- modules/stake.js
+++ modules/stake.js
@@ -15,7 +15,7 @@ class Stake {
       'sec-fetch-dest': 'empty',
       'sec-fetch-mode': 'cors',
       'sec-fetch-site': 'same-origin',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
       'x-access-token': access_token,
       'Cookie': cookie
     };
