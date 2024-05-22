839e6c1b35865264ff5ce973f658874334334b50
Glitch (x0p) <none>
2024-05-22 12:18:08 +0000

    🌸🐄 Checkpoint ./events/shop-events/restock.js:62000883/2021 ./events/shop-events/index.js:62000883/34

--- events/shop-events/index.js
+++ events/shop-events/index.js
@@ -2,5 +2,8 @@ export * from './add_item.js';
 export * from './edit_item.js';
 export * from './delete_item.js';
 
+export * from './restock.js';
+
+
 export * from './set_vouch.js';
 export * from './set_buyer.js';
---
+++ events/shop-events/restock.js
@@ -0,0 +1,81 @@
+import discord from 'discord.js';
+import newModal from './newModal.js';
+import newItemSelect from './newItemSelect.js';
+
+
+export async function restock(interaction, shop) {
+  let i;
+
+  try {
+    i = await newItemSelect(interaction, shop);
+  } catch (error) {
+    const embed = new discord.EmbedBuilder()
+      .setColor('Red')
+      .setTitle('失敗')
+      .setDescription('タイムアウトしました。');
+
+    interaction.editReply({ embeds: [embed], components: [] });
+  }
+
+  const itemName = i.values[0]
+
+
+  const modal = newModal({
+    id: 'modal',
+    title: '商品編集',
+    input: [
+      {
+        label: '商品名',
+        id: 'name',
+        style: discord.TextInputStyle.Short
+      },
+      {
+        label: '値段',
+        id: 'price',
+        style: discord.TextInputStyle.Short
+      }
+    ]
+  });
+
+  await i.showModal(modal);
+  let response;
+
+  try {
+    response = await i.awaitModalSubmit({ time: 180000 });
+  } catch (error) {
+    await interaction.followUp({ content: 'タイムアウトしました。', ephemeral: true });
+    return;
+  }
+
+  const inputName = response.fields.getTextInputValue('name');
+  const inputPrice = response.fields.getTextInputValue('price');
+
+  const index = shop.item.findIndex(element => element.name === itemName);
+
+  if (index === -1) {
+    await response.reply({ content: '商品が見つかりませんでした。', ephemeral: true });
+    return;
+  }
+
+  if (isNaN(parseInt(inputPrice)) || 999999 < parseInt(inputPrice)) {
+    await response.reply({ content: '無効な値段です。', ephemeral: true });
+    return;
+  }
+
+  const validRanges = inputName === itemName ? 1 : 0;
+
+  if (shop.item.filter(element => element.name === inputName) > validRanges) {
+    await response.reply({ content: '既に同じ名前の商品があります。', ephemeral: true });
+    return;
+  }
+
+  shop.item[index].name = inputName;
+  shop.item[index].price = inputPrice;
+
+  const embed = new discord.EmbedBuilder()
+    .setColor('Green')
+    .setTitle('商品編集')
+    .setDescription(`商品名: ${inputName}\n値段: ${inputPrice}円`);
+  
+  await response.reply({ embeds: [embed], ephemeral: true });
+};
