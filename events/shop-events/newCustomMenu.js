2613216b6ba2e5dd25afaea36a18f99e2eb9911c
Glitch (x0p) <none>
2024-05-22 01:00:27 +0000

    👖🕷 Checkpoint ./events/shop-events/newCustomMenu.js:62000883/877

---
+++ events/shop-events/newCustomMenu.js
@@ -0,0 +1,32 @@
+import discord from 'discord.js';
+
+function UUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 3 | 8);
+    return v.toString(16);
+  });
+}
+
+export default async function(interaction, shop, select) {
+  const randomId = UUID();
+
+  const menu = new discord[`${select}SelectMenuBuilder`]()
+    .setCustomId(randomId)
+    .setPlaceholder(`${select}を選択`);
+
+  const row = new discord.ActionRowBuilder()
+    .addComponents(menu);
+
+  const message = await interaction.reply({ components: [row], ephemeral: true });
+
+  return new Promise((resolve, reject) => {
+    async function onFunc(i) {
+      if (i.customId !== randomId) return;
+      interaction.client.off('interactionCreate', onFunc);
+      resolve(i);
+    }
+
+    interaction.client.on('interactionCreate', onFunc);
+  });
+};
