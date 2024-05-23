6545f778a5ab27a184e1001af08cee2a338b6e42
Glitch (x0p) <none>
2024-05-23 02:22:07 +0000

    🍳🏚 Checkpoint ./shop/1097780939368714310.json:62000883/68 ./events/shop-events/restock.js:62000883/1

--- events/shop-events/restock.js
+++ events/shop-events/restock.js
@@ -59,5 +59,5 @@ export async function restock(interaction, shop) {
     .setColor('Green')
     .setTitle('在庫追加')
 
-  await response.reply({ embeds: [embed], files: [file], phemeral: true });
+  await response.reply({ embeds: [embed], files: [file], ephemeral: true });
 };
