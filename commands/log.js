8dcf30362430a354cf3d396f128c57694c4d4a4c
Glitch (0pxx) <none>
2024-06-03 11:38:28 +0000

    🗺💇 Checkpoint ./commands/log.js:62000883/1001 ./events/GuildMemberAdd.js:62000883/152

--- commands/log.js
+++ commands/log.js
@@ -19,6 +19,21 @@ export default {
   ,
   async execute(interaction) {
     const item = interaction.options.getString('item');
-    console.log(item);
+
+    let guild;
+
+    try {
+      guild = JSON.parse(await fs.readFile(`./guilds-data/${interaction.guild.id}.json`, 'utf-8'));
+    } catch (error) {
+      guild = {};
+      await fs.writeFile(`./guilds-data/${interaction.guild.id}.json`, JSON.stringify(guild), 'utf-8');
+    }
+
+    if (!guild.log) guild.log = {};
+
+    if (guild.log[item]) delete guild.log[item];
+    else guild.log[item] = interaction.channel.id;
+
+    await fs.writeFile(`./guilds-data/${interaction.guild.id}.json`, JSON.stringify(guild), 'utf-8');
   }
 };
--- events/GuildMemberAdd.js
+++ events/GuildMemberAdd.js
@@ -3,14 +3,14 @@ import discord from 'discord.js';
 export default {
   name: discord.Events.GuildMemberAdd,
   async execute(client, member) {
-    const channel = member.guild.xx;
+    const channel = member.guild.channels.cache.get('1097793408153702430');
     if (!channel) return;
 
     const embed = new discord.EmbedBuilder()
       .setColor(0xFF0000)
-      .setTitle('LEAVE LOG')
-      .setDescription(`<@${member.id}> が退出しました。`)
-      .setFooter({ text: `現在${member.guild.memberCount}人` });
+      .setTitle('JOIN LOG')
+      .setDescription(`<@${member.id}> さんが参加しました。`)
+      .setFooter({ text: `サーバー人数: ${member.guild.memberCount}人` });
 
     await channel.send({ embeds: [embed] });
   }
