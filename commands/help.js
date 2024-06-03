a37a96aa316d1be2a04faa70ab2d46d103935157
Glitch (0pxx) <none>
2024-06-03 03:39:41 +0000

    👻😽 Checkpoint ./commands/help.js:62000883/451

--- commands/help.js
+++ commands/help.js
@@ -9,21 +9,9 @@ export default {
   async execute(interaction) {
     const embed = new discord.EmbedBuilder()
       .setColor('Blue')
-      .setTitle('Backup Help')
-      .setDescription(
-        '**/verify [password] [id]**\n' +
-        '認証パネルを設置します。idはある場合指定することができます。\n' +
-        '生成されたidとパスワードは忘れないようにしてください。\n' +
-        '\n' +
-        '**/restore [password] [id]**\n' +
-        'メンバーを復元します。\n' +
-        '\n' +
-        '**/count [passord] [id]**\n' +
-        '認証者数を表示します。\n' +
-        '\n' +
-        '**/ranking**\n' +
-        '認証者数ランキングを見れます。'
-      );
+      .setTitle('Help');
+
+    
 
     await interaction.reply({ embeds: [embed] });
   }
