61e20b33d6d009f675163af14e2f1f55a2bc7382
Glitch (0pxx) <none>
2024-06-03 23:05:13 +0000

    🎃🍎 Checkpoint ./server.js:62000883/730 ./commands/test/index.js:62000883/500

--- commands/test/index.js
+++ commands/test/index.js
@@ -1,11 +1,17 @@
 import discord from 'discord.js';
 
-const commands = {};
+const commands = [];
 
 
 const data = new discord.SlashCommandBuilder()
   .setName('backup')
   .setDescription('backup')
+
+
+commands.forEach(async (element) => {
+  const command = (await import(`filePath`));
+});
+
 
 
 import verify from './verify.js';
@@ -16,6 +22,16 @@ data.options.push(verify);
 
 
 
+
+console.log(
+  new discord.SlashCommandBuilder()
+    .setName('hello')
+    .setDescription('Hi Command')
+
+  .addSubcommand( subcommand => subcommand
+    .setName('subc')
+  )
+)
 
 
 export default {
