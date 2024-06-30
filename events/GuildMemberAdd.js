e6920d96bdac7ac86d9535244226240fb25e5fc1
Glitch (0pxx) <none>
2024-06-30 03:37:50 +0000

    👾🐑 Checkpoint ./events/GuildMemberAdd.js:62000883/534 ./events/GuildMemberRemove.js:62000883/1120

--- events/GuildMemberAdd.js
+++ events/GuildMemberAdd.js
@@ -3,7 +3,18 @@ import discord from 'discord.js';
 export default {
   name: discord.Events.GuildMemberAdd,
   async execute(client, member) {
-    if (member.guild.id === '') { //shop
+    if (member.guild.id === '1256799721230565488') { // shop
+      const channel = member.guild.channels.cache.get('1256812424137539684');
+
+      const embed = new discord.EmbedBuilder()
+        .setColor('Green')
+        .setTitle('参加通知')
+        .setDescription(`<@${member.id}> さんが参加しました。`)
+        .setThumbnail(member.user.displayAvatarURL())
+        .setFooter({ text: `サーバー人数: ${member.guild.memberCount}` })
+        .setTimestamp();
+
+      await channel.send({ embeds: [embed] });
     }
 
     const channel = member.guild.channels.cache.get('1097793408153702430');
--- events/GuildMemberRemove.js
+++ events/GuildMemberRemove.js
@@ -3,6 +3,20 @@ import discord from 'discord.js';
 export default {
   name: discord.Events.GuildMemberRemove,
   async execute(client, member) {
+    if (member.guild.id === '1256799721230565488') { // shop
+      const channel = member.guild.channels.cache.get('1256812424137539684');
+
+      const embed = new discord.EmbedBuilder()
+        .setColor('Red')
+        .setTitle('離脱通知')
+        .setDescription(`<@${member.id}> さんが離脱しました。`)
+        .setThumbnail(member.user.displayAvatarURL())
+        .setFooter({ text: `サーバー人数: ${member.guild.memberCount}` })
+        .setTimestamp();
+
+      await channel.send({ embeds: [embed] });
+    }
+
     const channel = member.guild.channels.cache.get('1097793408153702430');
     if (!channel) return;
 
