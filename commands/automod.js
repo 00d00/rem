3f753cb411d8921793f73906b640d42637cf1a8f
Glitch (0pxx) <none>
2024-07-01 23:59:25 +0000

    🚉🏜 Checkpoint ./commands/automod.js:62000883/459 ./automod/0.json:62000883/192

--- commands/automod.js
+++ commands/automod.js
@@ -1,4 +1,5 @@
 import discord from 'discord.js';
+import fs from 'fs/promises';
 
 export default {
   data: new discord.SlashCommandBuilder()
@@ -7,6 +8,18 @@ export default {
     .setDefaultMemberPermissions(discord.PermissionFlagsBits.Administrator)
   ,
   async execute(interaction) {
+    let data;
+    try {
+      data = await fs.readFile(`./automod/${interaction.guild.id}.json`);
+    } catch (error) {
+      data = {
+        log: null,
+        keywords: [],
+        mentionable: 0,
+        
+      };
+    }
+
     const embed = new discord.EmbedBuilder()
       .setColor('Blue')
       .setTitle('Automod Settings')
