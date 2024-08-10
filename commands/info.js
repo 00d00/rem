72e8674d359d9918722d749c842eb3122684f343
Glitch (0pxx) <none>
2024-08-10 08:15:50 +0000

    🏟🚃 Checkpoint ./commands/info.js:62000883/666 ./commands/calculator.js:62000883/510

--- commands/info.js
+++ commands/info.js
@@ -13,9 +13,22 @@ export default {
       .addFields(
         { name: 'Server Count', value: `\`\`\`${client.guilds.cache.size}\`\`\``, inline: true },
         { name: 'Uptime', value: `\`\`\`${Math.floor(client.uptime / 1000)} seconds\`\`\``, inline: true },
-        { name: 'Ping', value: `${client.ws.ping} ms`, inline: true }
+        { name: 'Ping', value: `Pinging...`, inline: true },
       );
 
+    const start = Date.now();
     await interaction.reply({ embeds: [embed] });
+    const ping = Date.now() - start;
+
+    const newEmbed = new EmbedBuilder()
+      .setColor('Blue')
+      .setTitle('Aces BOT INFO')
+      .addFields(
+        { name: 'Server Count', value: `\`\`\`${client.guilds.cache.size}\`\`\``, inline: true },
+        { name: 'Uptime', value: `\`\`\`${Math.floor(client.uptime / 1000)} seconds\`\`\``, inline: true },
+        { name: 'Ping', value: `\`\`\`${ping} ms\`\`\``, inline: true },
+      );
+
+    await interaction.editReply({ embeds: [newEmbed] });
   }
 };
